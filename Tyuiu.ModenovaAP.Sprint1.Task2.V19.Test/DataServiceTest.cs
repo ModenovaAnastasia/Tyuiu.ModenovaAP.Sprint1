f9ae83ef2f9ac1737e741bd7fa2e3f0ae1429358
Tyuiu.ModenovaAP@outlook.com <fanriall@mail.ru>
2023-09-22 20:48:11 +0500

    Task2

---
+++ Tyuiu.ModenovaAP.Sprint1.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ModenovaAP.Sprint1.Task2.V19.Lib;
+
+namespace Tyuiu.ModenovaAP.Sprint1.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.ConvertInchToKm(x);
+            Assert.AreEqual(0.0508, res);
+        }
+    }
+}
