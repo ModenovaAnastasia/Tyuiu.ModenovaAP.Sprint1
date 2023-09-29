ebae14736c8cdf4dc643146ebdde9e6f2828aa39
Tyuiu.ModenovaAP@outlook.com <fanriall@mail.ru>
2023-09-29 23:45:42 +0500

    Task7

---
+++ Tyuiu.ModenovaAP.Sprint1.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ModenovaAP.Sprint1.Task7.V18.Lib;
+
+namespace Tyuiu.ModenovaAP.Sprint1.Task7.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression1()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double y = 1;
+            double wait = -0.5;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ModenovaAP.Sprint1.Task7.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ModenovaAP.Sprint1.Task7.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ModenovaAP.Sprint1.Task7.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("17032ab9-be1e-49de-a0ad-f0b596b3213d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
